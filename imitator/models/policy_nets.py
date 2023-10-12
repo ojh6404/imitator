6fc53421a76742b3910069e6248054659d046c02
Jihoon Oh <ojh6404@gmail.com>
2023-10-13 06:00:44 +0900

    tweak

--- imitator/models/policy_nets.py
+++ imitator/models/policy_nets.py
@@ -209,8 +209,10 @@ class MLPActor(Actor):
         self,
         batch: Dict[str, Union[torch.Tensor, Dict[str, torch.Tensor]]],
     ) -> torch.Tensor:
-        obs_latents = self.nets["obs_encoder"](batch["obs"])
-        outputs = self.nets["mlp_decoder"](obs_latents)
+        obs_latents = self.nets["obs_encoder"](batch["obs"]) # [B, T, D]
+        mlp_decoder_outputs = self.nets["mlp_decoder"](obs_latents) # [B, T, D]
+        actions = batch.get("actions", None)
+        outputs = self.mlp_decoder_postprocess(mlp_decoder_outputs, actions)
         return outputs
 
     def forward(
@@ -223,9 +225,11 @@ class MLPActor(Actor):
         self,
         batch: Dict[str, Union[torch.Tensor, Dict[str, torch.Tensor]]],
     ) -> Tuple[torch.Tensor, torch.Tensor]:
-        actions = self.forward(batch)["predictions"]
+        batch = TensorUtils.to_sequence(batch) # [B, 1, D]
+        actions = self.forward(batch)["predictions"] # [B, D]
+        actions = TensorUtils.squeeze(actions, 1) # [B, D]
         actions = self.action_modality.unprocess_obs(actions)  # numpy ndarray
-        return actions  # [B, D], last timestep cause it uses framestack
+        return actions
 
 
 class RNNActor(Actor):
@@ -434,7 +438,7 @@ class TransformerActor(Actor):
         self,
         batch: Dict[str, Union[torch.Tensor, Dict[str, torch.Tensor]]],
     ) -> Tuple[torch.Tensor, torch.Tensor]:
-        actions = self.forward(batch)["predictions"]
+        actions = self.forward(batch)["predictions"] # [B, T, D]
         actions = self.action_modality.unprocess_obs(actions)  # numpy ndarray
         if self.supervise_all_steps:  # get last timestep
             actions = actions[:, -1, :]
